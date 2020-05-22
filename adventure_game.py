1bd88eea20663b5f29d4f54b7e808e8850081c40
Kaniganti Sai Tarun <saitarunkaniganti@gmail.com>
2020-05-22 17:06:08 +0530

    Update adventure_game.py

--- adventure_game.py
+++ adventure_game.py
@@ -1,112 +1,127 @@
-import time
 import random
+import time
 
 
-def print_pause(msg_to_print):
-    print(msg_to_print)
-    time.sleep(2)
+def print_sleep(message, wait_time):
+    print(message)
+    time.sleep(wait_time)
 
-
-def intro(item, option):
-    print_pause("You find yourself standing in an open field, filled "
-                "with grass and yellow wildflowers.\n")
-    print_pause("Rumor has it that a " + option + " is somewhere around "
-                "here, and has been terrifying the nearby village.\n")
-    print_pause("In front of you is a house.\n")
-    print_pause("To your right is a dark cave.\n")
-    print_pause("In your hand you hold your trusty (but not very "
-                "effective) dagger.\n")
-
-
-def cave(item, option):
-    if "sward" in item:
-        print_pause("\nYou peer cautiously into the cave.")
-        print_pause("\nYou've been here before, and gotten all"
-                    " the good stuff. It's just an empty cave"
-                    " now.")
-        print_pause("\nYou walk back to the field.\n")
-    else:
-        print_pause("\nYou peer cautiously into the cave.")
-        print_pause("\nIt turns out to be only a very small cave.")
-        print_pause("\nYour eye catches a glint of metal behind a "
-                    "rock.")
-        print_pause("\nYou have found the magical Sword of Ogoroth!")
-        print_pause("\nYou discard your silly old dagger and take "
-                    "the sword with you.")
-        print_pause("\nYou walk back out to the field.\n")
-        item.append("sward")
-    field(item, option)
-
-
-def house(item, option):
-    print_pause("\nYou approach the door of the house.")
-    print_pause("\nYou are about to knock when the door "
-                "opens and out steps a " + option + ".")
-    print_pause("\nEep! This is the " + option + "'s house!")
-    print_pause("\nThe " + option + " attacks you!\n")
-    if "sward" not in item:
-        print_pause("You feel a bit under-prepared for this, "
-                    "what with only having a tiny dagger.\n")
-    while True:
-        choice2 = input("Would you like to (1) fight or (2) "
-                        "run away?")
-        if choice2 == "1":
-            if "sward" in item:
-                print_pause("\nAs the " + option + " moves to attack, "
-                            "you unsheath your new sword.")
-                print_pause("\nThe Sword of Ogoroth shines brightly in "
-                            "your hand as you brace yourself for the "
-                            "attack.")
-                print_pause("\nBut the " + option + "takes one look at "
-                            "your shiny new toy and runs away!")
-                print_pause("\nYou have rid the town of the " + option +
-                            ". You are victorious!\n")
-            else:
-                print_pause("\nYou do your best...")
-                print_pause("but your dagger is no match for the "
-                            + option + ".")
-                print_pause("\nYou have been defeated!\n")
-            play_again()
-            break
-        if choice2 == "2":
-            print_pause("\nYou run back into the field. "
-                        "\nLuckily, you don't seem to have been "
-                        "followed.\n")
-            field(item, option)
-            break
-
-
-def field(item, option):
-    print_pause("Enter 1 to knock on the door of the house.")
-    print_pause("Enter 2 to peer into the cave.")
-    print_pause("What would you like to do?")
-    while True:
-        choice1 = input("(Please enter 1 or 2.)\n")
-        if choice1 == "1":
-            house(item, option)
-            break
-        elif choice1 == "2":
-            cave(item, option)
-            break
+def combat(weapon):
+    print_sleep(f"The {enemy} attacks you!", 2)
+    if weapon == "batlef":
+        print_sleep(f"You a chosen warrior. Last of a dying breed, "
+                    "your armed with a {weapon}.", 2)
+    choice = input("Would you like to (1) bust some heads or (2) "
+                   "run away like a coward?")
+    if choice == '1':
+        if weapon == "batlef":
+            print_sleep(f"You do your best...", 1)
+            print_sleep(f"but your {weapon} is no match for the {enemy}.", 2)
+            print_sleep(f"You have been defeated!""", 2)
+        elif weapon == "sword":
+            print_sleep(f"As the {enemy} moves to "
+                        "attack, you unsheath your new sword.", 2)
+            print_sleep(f"The sword shines brightly "
+                        "in your hand as you prepare "
+                        "yourself for any attack.", 3)
+            print_sleep(f"But the {enemy} takes one "
+                        "look at your sword and prepares to fight!", 3)
+            print_sleep(f"You have rid the town of the {enemy}. "
+                        "You are victorious!", 3)
+        elif weapon == "ak-47":
+            print_sleep(f"As the {enemy} tries to grab your ak-r7, "
+                        "you pull the trigger.", 2)
+            print_sleep(f"The gold colored rifle shines brightly "
+                        "in your warrior hands!", 3)
+            print_sleep(f"The {enemy} tries to grab the Marine "
+                        "but he gets a chest full of lead!", 3)
+            print_sleep(f"The {enemy} wakes from his dream screaming. "
+                        "He can't believe he's alive!", 3)
+    elif choice == '2':
+        print_sleep("You run back into the field. Luckily, you don't "
+                    "seem to have been followed.", 2)
+        where_to()
+    elif choice == '3':
+        print_sleep("End Game", 3)
+        where_to()
 
 
 def play_again():
-    again = input("Would you like to play again? (y/n)").lower()
-    if again == "y":
-        print_pause("\n\n\nExcellent! Restarting the game ...\n\n\n")
-        play_game()
-    elif again == "n":
-        print_pause("\n\n\nThanks for playing! See you next time.\n\n\n")
-    else:
-        play_again()
+    choice = ''
+    while choice not in ['y', 'n']:
+        choice = input("Would you like to play again? (y/n)")
+        if choice == 'n':
+            print_sleep("Thanks for playing! See you next time.", 2)
+            return 'game_over'
+        elif choice == 'y':
+            print_sleep("Excellent! Restarting the game ...", 2)
+            weapon = 'batlef'
+            return 'running'
 
 
-def play_game():
-    item = []
-    option = random.choice(["wicked fairie", "pirate", "dragon", "troll",
-                            "gorgon"])
-    intro(item, option)
-    field(item, option)
+def intro():
+    print_sleep("You find yourself standing in the streets of "
+                "Baltimore, the concrete jungle.", 3)
+    print_sleep(f"Command has said that a {enemy} is somewhere "
+                "around here, and has been terrifying the city.", 3)
+    print_sleep("In front of you is an abandoned building.", 2)
+    print_sleep("To your right is a dark alley.", 2)
+    print_sleep(f"In your hand you hold your trusty {weapon}.", 2)
 
 
-play_game()
+def where_to():
+    print_sleep("", 1)
+    print_sleep("Enter 1 to knock on the door of the abandoned buiding.", 2)
+    print_sleep("Enter 2 to peer into the alley.", 2)
+    print_sleep("What would you like to do?", 0)
+    choice = ''
+    while choice not in ['1', '2']:
+        choice = input("(Please enter 1 or 2.)\n")
+
+    if choice == '1':
+        building()
+    # elif choice == '2':
+        # alley()
+
+
+def building():
+    print_sleep("You approach the door of the building.", 2)
+    print_sleep(f"You are about to knock when the door "
+                "opens and out steps a {enemy}.", 2)
+    print_sleep(f"Ahhhh! That is the {enemy}'s building!", 2)
+    combat(weapon)
+
+
+def alley_visited():
+    global alley_visited
+    global weapon
+    print_sleep("You look down the alley.", 2)
+    if alley_visited:
+        print_sleep("You've been here before, and gotten "
+                    "all the good stuff. It's just an empty alley now.", 2)
+    elif alley_visited is False:
+        print_sleep("It turns out to be only a very small "
+                    "abandoned building.", 2)
+        print_sleep("Your eye catches a glint of metal behind a rock.", 2)
+        print_sleep("You have found the ak-47 of your dreams!", 2)
+        print_sleep(f"You discard your silly old {weapon} "
+                    "and take the ak-47 with you.", 2)
+        weapon = "sword"
+    alley_visited = True
+    print_sleep("You walk back out to the field. You wipe of "
+                "the sweat and drink your gatorade", 2)
+    where_to()
+
+
+game_state = 'running'
+while game_state == 'running':
+
+    enemies = ['nokton', 'kaylon', 'vulcan', 'klingon', 'moklan']
+    enemy = random.choice(enemies)
+    weapon = 'batlef'
+    alley_visited = False
+
+    intro()
+    where_to()
+
+    game_state = play_again()
